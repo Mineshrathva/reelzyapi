142400a51f29be531f97516d4f536f9fb2bf093f
Mineshrathva <110728242+Mineshrathva@users.noreply.github.com>
2026-01-07 22:15:31 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -49,7 +49,7 @@ app.use("/api/upload", uploadRouter);
 app.use("/api/reels_action", reelActionsRouter);
 app.use("/api/stories_action", storyActionsRouter);
 app.use("/api/story-details", storyDetailsRoutes);
-app.use("/api/users/profile", profileOtherRoutes);
+app.use("/api/profile", profileOtherRoutes);
 app.use("/api/stories", storySeenRouter);
 
 app.use(errorHandler);
