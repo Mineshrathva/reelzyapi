a58db53301d320ae5b29f42c1484006a86f82a54
Mineshrathva <110728242+Mineshrathva@users.noreply.github.com>
2026-01-12 17:13:16 +0530

    Delete src/types/src/types directory

--- src/types/src/types/express.ts
+++
@@ -1,9 +0,0 @@
-import { UserType } from "../models/UserType"; // यदि हो तो, नहीं है तो ignore
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: any; // or your exact user type
-    }
-  }
-}
